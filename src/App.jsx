f1d457453a0ee27a21b21809402be70d12b9df3c
Arin Bagul <prototypearin@gmail.com>
2025-01-27 23:54:25 +0530

    Corrected the import in App.js, pages imported with small file name

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import abLogo from "/abLogo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import Error from "./pages/Error";
 import Navbar from "./components/Navbar";
 
