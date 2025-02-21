8c4588f362bd7de8375c97324aa90073ac9b4939
Arin Bagul <prototypearin@gmail.com>
2025-02-21 14:10:37 +0530

    firebase fix 4 (test) tokens exposed...?

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -7,13 +7,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyCbZ1i0lulaW_jghzSP8CzddBvA0BquhV8",
+  authDomain: "ab-productions-553c6.firebaseapp.com",
+  projectId: "ab-productions-553c6",
+  storageBucket: "ab-productions-553c6.firebasestorage.app",
+  messagingSenderId: "495784159975",
+  appId: "1:495784159975:web:54fbef852fcdf3deefd042",
+  measurementId: "G-9VRZ1GXTY7",
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_APP_ID,
+  // measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
