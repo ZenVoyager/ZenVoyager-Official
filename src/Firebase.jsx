6092ce63182c90283d5dcb254ce9094143785cfd
Arin Bagul <prototypearin@gmail.com>
2025-02-21 13:52:54 +0530

    firebase fix 2...

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -18,6 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
-export { db };
+export default app;
